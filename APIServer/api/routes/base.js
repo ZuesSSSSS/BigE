df9c45af64285a8afe15d0ce47b4ba6b1bda50fa
Brendan <brendan.angerman@gmail.com>
2020-02-29 12:09:30 -0700

    update endpoint

--- APIServer/api/routes/base.js
+++ APIServer/api/routes/base.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const shell = require('shelljs')
 
 router.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'Base Endpoint'
   })
 })
+
+router.get('/git-update', (req, res, next) => {
+  if (!shell.which('git')) {
+    shell.echo('Sorry, this script requires git')
+    shell.exit(1)
+  }
+
+  var update = shell.exec('git fetch && git pull')
+
+  res.status(200).json({
+    message: update,
+  })
+  
+});
 
 module.exports = router
