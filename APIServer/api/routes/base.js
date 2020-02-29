1f5aa42ca87f23d7d52783879ecab674da664dae
Brendan <brendan.angerman@gmail.com>
2020-02-29 12:25:32 -0700

    test

--- APIServer/api/routes/base.js
+++ APIServer/api/routes/base.js
@@ -14,8 +14,16 @@ router.get('/git-update', (req, res, next) => {
     shell.exit(1)
   }
 
+  // Reset to head
+  shell.exec('git reset --hard origin/master');
+
+  // Pull new changes
   var git = shell.exec('git fetch && git pull')
+  
+  // Install the (maybe) added npm packages
   var npm = shell.exec('npm i');
+
+  // Restart the process manager ( node server )
   var processManager = shell.exec('pm2 restart index');
 
   res.status(200).json({
