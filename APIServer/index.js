bc9cc8b6231a1a12e4c52c9c3245fccd02477a9d
Brendan <brendan.angerman@gmail.com>
2020-02-29 11:35:53 -0700

    test

--- APIServer/index.js
+++ APIServer/index.js
@@ -1,8 +1,8 @@
 const http = require('http')
 const app = require('./app')
 
-const port = process.env.PORT;
+const port = 3000;
 
 app.listen(port, () => {
-  console.log('Example app listening on port 8000!')
+  console.log(`Example app listening on port ${port}!`)
 });
