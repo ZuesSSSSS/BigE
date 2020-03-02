5eb5de6a239ee85d2e1e15851d4f8363103e629d
Brendan <brendan.angerman@gmail.com>
2020-03-01 21:11:26 -0700

    some login stuff

--- website/src/pages/auth/login/Login.js
+++ website/src/pages/auth/login/Login.js
@@ -1,5 +1,51 @@
 import React from "react";
 
-export default function Login() {
-  return <h2>Login</h2>;
+class Login extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    event.preventDefault()
+    console.log( event )
+  }
+
+  handleSubmit() {
+    console.log(`Form submitted with username:${this.state.username}, password:${this.state.password}`)
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={this.handleSubmit}>
+
+          <label>
+            Username:
+            <input type="text" value={this.state.username} onChange={this.handleChange} />
+          </label>
+
+          <label>
+            Password:
+            <input type="password" value={this.state.password} onChange={this.handleChange} />
+          </label>
+
+          <input type="submit" value="Submit" />
+
+        </form>
+      </div>
+    )
+  }
 }
+
+export default Login
