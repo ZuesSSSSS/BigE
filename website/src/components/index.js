8e6f0a87ddbc93833872a606b1c97d923fe1dd5a
Brendan <brendan.angerman@gmail.com>
2020-03-08 22:05:31 -0600

    resolved merge conflict

--- website/src/components/index.js
+++ website/src/components/index.js
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
 import Header from './Header';
-
-export {
-  Header
-=======
+  
 import AppContainer from './AppContainer/AppContainer';
 
 import ProductCard from './ProductCard/ProductCard';
 import Cart from './Cart/Cart';
 
 export {
+  Header,
   AppContainer,
 
   ProductCard,
   Cart,
->>>>>>> laravel-lumen-api
 }
